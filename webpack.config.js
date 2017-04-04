134fe13072daabab86bdcdbd1077b533915d033f
John Colella <john.colella@policygenius.com>
2017-04-04 12:53:50 -0400

    Update webpack to v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/public/index.html',
   filename: 'index.html',
@@ -14,30 +16,111 @@ module.exports = {
     './app/public/index.js'
   ],
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve( __dirname, './dist' ),
     filename: 'index_bundle.js'
   },
   module: {
-    preLoaders: [
-      { test: /\.js$/,  exclude: /node_modules/, loader: "eslint-loader" }
-    ],
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'react-hot' },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: [ 'react', 'es2015' ] } },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract( 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader' ) },
-      { test: /\.module\.scss$/, loader: ExtractTextPlugin.extract( ['style-loader?sourceMap', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader', 'sass-loader?sourceMap'] ) },
-      { test: /\.scss$/, exclude: /\.module\.scss$/, loader: ExtractTextPlugin.extract( ['style-loader?sourceMap', 'css-loader!postcss-loader', 'sass-loader?sourceMap'] ) }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              configFile: './.eslintrc'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'react-hot-loader'
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: [ 'react', 'es2015' ] }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => {
+                  return [
+                    require('postcss-modules-valus'),
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                importLoaders: 1
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader?importLoaders=1',
+              options: {
+                modules: true,
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => {
+                  return [
+                    require('postcss-modules-values'),
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                sourceComments: true
+              }
+            }
+          ]
+        })
+      },
     ]
-  },
-  postcss: [
-    require('postcss-modules-values'),
-    require('autoprefixer')
-  ],
-  eslint: {
-    configFile: './.eslintrc'
   },
   plugins: [
     HTMLWebpackPluginConfig,
-    new ExtractTextPlugin('styles.css')
+    new ExtractTextPlugin({
+      filename: 'styles.css',
+      allChunks: true
+    })
   ]
 };
