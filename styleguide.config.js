28c9d65f57d19f011015b87755571c05c026518e
Joshua Clark <josh@policygenius.com>
2017-04-21 17:33:06 -0400

    Add versioning and custom deploys to build

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 const webpackConfig = isProd ?
-  require('./webpack.config.prod.js') :
+  require('./webpack.config.base.js') :
   require('./webpack.config.dev.js');
 
 module.exports = {
