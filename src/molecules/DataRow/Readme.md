f89f1ca6b9a9af510379911663a2fccf185bf0db
Patrick Kim <patrick.kim@policygenius.com>
2017-05-01 11:04:12 -0400

    Add DataRow

---
+++ src/molecules/DataRow/Readme.md
@@ -0,0 +1,25 @@
+DataRow Example:
+
+```example
+
+    <div style={{ margin: '15px' }}>
+      <DataRow
+        label='Some Data'
+        value={17.76}
+        description='Make things happen!'
+      />
+
+      <DataRow
+        label='More Data'
+        value={19.73}
+      />
+
+      <DataRow
+        label='More Data'
+        value={19.73}
+        variant='final'
+      />
+    </div>
+
+
+```
---
+++ src/molecules/DataRow/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import Tooltip from 'atoms/Tooltip';
+
+import styles from './data_row.module.scss';
+
+function DataRow(props) {
+  const {
+    className,
+    variant,
+    label,
+    value,
+    description,
+  } = props;
+
+  return (
+    <div className={classnames(styles['data-row'], styles[variant], className)}>
+      <span className={classnames(styles['label'])}>
+        { label }
+        { description && <Tooltip className={styles['tip']}>{ description }</Tooltip> }
+      </span>
+
+      <span className={classnames(styles['value'])}>
+        { value }
+      </span>
+    </div>
+  );
+}
+
+
+DataRow.propTypes = {
+  /**
+   * This prop will add a new className to any inherent classNames
+   * provided in the component's index.js file.
+   */
+  className: PropTypes.string,
+  /**
+   * Possible variants are: `final` or (default)
+   */
+  variant: PropTypes.string,
+  /**
+   * label/name for data
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * value/name for data could be a string or a number.
+   */
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired,
+  /**
+   * optional helper text describing this data.
+   */
+  description: PropTypes.string,
+};
+
+DataRow.defaultProps = {
+  // Place any default props here.
+};
+
+export default DataRow;
