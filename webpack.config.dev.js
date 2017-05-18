72f7ec80422a348efb3c224eee52d3ba8e3d441e
Andrew Joia <drewdrewthis@gmail.com>
2017-05-18 16:23:42 -0400

    Update config for new react-styleguidist set up

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,13 +1,19 @@
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const path = require('path');
 
 const baseConfig = require('./webpack.config.base.js');
+
+const includePaths = [
+  path.resolve(__dirname, 'src'),
+  path.resolve(__dirname, 'styleguide_assets'),
+];
 
 module.exports = baseConfig({
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: includePaths,
         enforce: 'pre',
         use: [
           {
@@ -21,7 +27,7 @@ module.exports = baseConfig({
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: includePaths,
         use: [
           {
             loader: 'react-hot-loader'
