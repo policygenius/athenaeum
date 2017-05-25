cd85863aa0d921cb415271debadeba84d224334c
Patrick Kim <patrick.kim@policygenius.com>
2017-05-25 12:29:40 -0400

    Fix failing spec

--- src/molecules/AgentCallout/__tests__/agent_callout.spec.js
+++ src/molecules/AgentCallout/__tests__/agent_callout.spec.js
@@ -7,7 +7,7 @@ import AgentCallout from '../index';
 describe('<AgentCallout />', () => {
 
   it('renders', () => {
-    const wrapper = shallow(<AgentCallout />);
+    const wrapper = shallow(<AgentCallout imgSrc='' />);
 
     expect(wrapper.length).to.equal(1);
   });
