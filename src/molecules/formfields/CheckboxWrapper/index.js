64b0bf55baa2d3d81f82b45807997981829bb324
John Colella <john.colella@policygenius.com>
2017-07-18 15:55:07 -0400

    Add toolltip to checkbox wrapper

--- src/molecules/formfields/CheckboxWrapper/index.js
+++ src/molecules/formfields/CheckboxWrapper/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
+import Tooltip from 'atoms/Tooltip';
 import styles from './checkbox_wrapper.module.scss';
+
+function renderTooltip(tooltip, className) {
+  if (typeof tooltip === 'string') {
+    return <Tooltip className={styles[className]}>{tooltip}</Tooltip>;
+  }
+
+  return <Tooltip className={styles[className]} onClick={tooltip} />;
+}
 
 function CheckboxWrapper( props ) {
   const {
     label,
     focused,
-    children
+    children,
+    tooltip,
   } = props;
 
   return (
     <div className={classnames(styles['checkbox-list'], focused && styles['focused'] )}>
-      { label && <label htmlFor='checkbox' className={styles['label']}>{label}</label> }
+      <div className={styles['header']}>
+        { label && <label htmlFor='checkbox' className={styles['label']}>{label}</label> }
+        { tooltip && renderTooltip(tooltip, 'tooltip') }
+      </div>
       <div className={styles['content']}>{ children }</div>
     </div>
   );
@@ -27,7 +40,15 @@ CheckboxWrapper.propTypes = {
   /*
   * Whether or not the checkbox wrapper is focused
   */
-  focused: PropTypes.bool
+  focused: PropTypes.bool,
+
+  /**
+   * Adds tooltip to wrapper
+   */
+  tooltip: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func
+  ]),
 };
 
 export default CheckboxWrapper;
