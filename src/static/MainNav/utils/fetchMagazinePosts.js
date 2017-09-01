0c4df29e238f37675159fa37e4ae6c6bd97a9b7c
John Colella <john.colella@policygenius.com>
2017-09-01 15:04:44 -0400

    Fix blog fetch

--- src/static/MainNav/utils/fetchMagazinePosts.js
+++ src/static/MainNav/utils/fetchMagazinePosts.js
@@ -1,10 +1,9 @@
 import 'whatwg-fetch';
 
 export const fetchPosts = (options = {}) => {
-  const includeTag = options.tag ? `,tags:${options.tag}` : '';
+  const includeTag = options.tag ? `%2Btags:[${options.tag}]` : '';
   const filter = `featured:${options.featured}${includeTag}`;
 
   return fetch(`https://www.policygenius.com/blog/ghost/api/v0.1/posts/?filter=${filter}&limit=${options.limit}&client_id=ghost-frontend&client_secret=8e87db316394`)
     .then(res => res.json());
 };
-
