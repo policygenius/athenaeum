84ac364e91050ead6f1232263de00b519a43842c
John Colella <john.colella@policygenius.com>
2017-09-01 23:25:38 -0400

    Update header highlighting and fix incorrect blog urls

---
+++ src/static/MainNav/data/urlMatchData.js
@@ -0,0 +1,20 @@
+const urlMatchData = [
+  {
+    path: 'insurance-checkup-and-advice',
+    active: 'insurance-checkup',
+  },
+  {
+    path: 'blog',
+    active: 'magazine',
+  },
+  {
+    path: 'about',
+    active: 'about',
+  },
+  {
+    path: 'careers',
+    active: 'about',
+  },
+];
+
+export default urlMatchData;
