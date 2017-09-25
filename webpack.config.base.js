2c31825af56afbb079757d9829eb3e447efcd13c
Andrew Joia <drewdrewthis@gmail.com>
2017-09-25 13:39:19 -0400

    Get RCL to work in IE

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,3 +1,5 @@
+process.traceDeprecation = true;
+
 const path = require('path');
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 const postCSSConfig = require('./postcss.config.js');
@@ -22,7 +24,10 @@ const baseRules = [
         // babel-loader will throw a deprecation warning
         // waiting for stable v7.0.0 to upgrade
         loader: 'babel-loader',
-        options: { presets: [ 'react', 'es2015', 'stage-2' ] }
+        options: {
+          presets: [ 'env', 'react', 'stage-2' ],
+          plugins: ['babel-polyfill']
+        }
       }
     ]
   },
@@ -110,7 +115,9 @@ module.exports = options => {
   };
 
   return {
-    entry: options.entry,
+    entry: [
+      'babel-polyfill'
+    ].concat(options.entry),
     output: options.output,
     externals: options.externals,
     devtool: 'source-map',
