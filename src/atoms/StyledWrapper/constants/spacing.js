139e3cff55a8bdd29cae4ba00d71a2a2a916573a
Andrew Joia <drewdrewthis@gmail.com>
2017-10-13 10:42:05 -0400

    Add spacing to StyledWrapper

---
+++ src/atoms/StyledWrapper/constants/spacing.js
@@ -0,0 +1,13 @@
+export const ru = num => `${num * 1.5}rem`;
+
+export default {
+  6: ru(0.25),
+  12: ru(0.5),
+  18: ru(0.75),
+  24: ru(1),
+  36: ru(1.5),
+  60: ru(2.5),
+  84: ru(3.5),
+  120: ru(5),
+  162: ru(6.75)
+};
