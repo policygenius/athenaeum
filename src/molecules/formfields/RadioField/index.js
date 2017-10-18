5f7c1ce9dbfb48e724d9c9ac2cd42064bec453d6
John Colella <john.colella@policygenius.com>
2017-10-18 16:15:22 -0400

    Finish adding radio group

--- src/molecules/formfields/RadioField/index.js
+++ src/molecules/formfields/RadioField/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { camelCase } from 'lodash';
+import camelCase from 'lodash/camelCase';
+import omit from 'lodash/omit';
 import classnames from 'classnames';
 
 import styles from './radio_field.module.scss';
@@ -15,30 +16,44 @@ function RadioField(props) {
 
   const classes = [
     styles['radio-field'],
-    input.value === radioValue && styleds['checked'],
-    input.value && input.value !== radioValue && styles['not-selected'],
     className,
+  ];
+
+  const labelClasses = [
+    styles['label'],
+    input.value === radioValue && styles['checked'],
+    input.value && input.value !== radioValue && styles['not-selected'],
   ];
 
   return (
     <label
       className={classnames(...classes)}
-      htmlFor={`radio-${camelCase(input.value)}`}
+      htmlFor={`radio-${camelCase(radioValue)}`}
     >
       <input
         type='radio'
         name={input.name}
-        id={`radio-${camelCase(input.value)}`}
+        id={`radio-${camelCase(radioValue)}`}
         className={classnames(styles['radio'])}
-        {...input}
+        onChange={() => input.onChange(radioValue)}
+        {...omit(input, 'onChange')}
       />
-      <span className={styles['label']}>{ label }</span>
+      <span className={classnames(...labelClasses)}>{ label }</span>
     </label>
   );
 }
 
 
 RadioField.propTypes = {
+  /**
+   * This prop will add a new className to any inherent classNames
+   * provided in the component's index.js file.
+   */
+  className: PropTypes.string,
+
+  /**
+   * label for field directly next to the button
+   */
   label: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.object
@@ -50,7 +65,15 @@ RadioField.propTypes = {
   /**
    * The props under the input key are passed from `redux-form` and spread into `<input />`.
    */
-  input: PropTypes.object.isRequired
+  input: PropTypes.object.isRequired,
+
+  /**
+   * Value for radio field
+   */
+  radioValue: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]).isRequired
 };
 
 export default RadioField;
