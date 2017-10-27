d2bda4e45564250f2a55533bfe64c5c5acf0126c
Drew McDrewface <andrew.joia@policygenius.com>
2017-10-27 19:16:45 -0400

    Update build for styled components

--- footer/index.js
+++ footer/index.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const Footer = require('./static/static.js');
+const ServerStyleSheet = require('styled-components').ServerStyleSheet;
 
-const html = ReactDOMServer.renderToStaticMarkup(React.createElement(Footer.default));
+const sheet = new ServerStyleSheet();
+
+const html = ReactDOMServer.renderToStaticMarkup(
+  sheet.collectStyles(React.createElement(Footer.default))
+);
+
+const css = sheet.getStyleTags();
 
 fs.writeFile('./footer/build/static-index.html', html, (err) => {
   if (err) {
@@ -15,3 +22,13 @@ fs.writeFile('./footer/build/static-index.html', html, (err) => {
   console.log('Successfully wrote Footer component to static-index.html');
 });
 
+console.log('CSS!!', css);
+
+fs.writeFile('./footer/build/assets/styled.css', css, (err) => {
+  if (err) {
+    console.log('ERR:', err);
+    return;
+  }
+
+  console.log('Successfully wrote Footer css to styled.css');
+});
--- main_nav/index.js
+++ main_nav/index.js
@@ -3,8 +3,15 @@ const fs = require('fs');
 const React = require('react');
 const ReactDOMServer = require('react-dom/server');
 const MainNavNew = require('./static/static.js');
+const ServerStyleSheet = require('styled-components').ServerStyleSheet;
 
-const html = ReactDOMServer.renderToStaticMarkup(React.createElement(MainNavNew.default));
+const sheet = new ServerStyleSheet();
+
+const html = ReactDOMServer.renderToStaticMarkup(
+  sheet.collectStyles(React.createElement(MainNavNew.default))
+);
+
+const css = sheet.getStyleTags();
 
 fs.writeFile('./main_nav/build/static-index.html', html, (err) => {
   if (err) {
@@ -15,3 +22,11 @@ fs.writeFile('./main_nav/build/static-index.html', html, (err) => {
   console.log('Successfully wrote MainNavNew component to static-index.html');
 });
 
+fs.writeFile('./main_nav/build/assets/styled.css', css, (err) => {
+  if (err) {
+    console.log('ERR:', err);
+    return;
+  }
+
+  console.log('Successfully wrote MainNavNew css to styled.css');
+});
