931c9fb0ca2da8df4332fc10b77514d743d42e1d
Drew McDrewface <andrew.joia@policygenius.com>
2017-11-15 18:27:36 -0500

    Fix icon and link list in footer

--- src/static/Footer/SocialMedia/index.js
+++ src/static/Footer/SocialMedia/index.js
@@ -3,34 +3,32 @@ import React from 'react';
 import Icon from 'atoms/Icon';
 import List from 'atoms/List';
 import LinkWrapper from 'atoms/LinkWrapper';
-import StyledWrapper from 'atoms/StyledWrapper';
 
-import { iconList } from './styles';
+import styles from './social-media.module.scss';
 import { socialMediaData } from '../data/socialMediaData';
 
 const SocialMedia = () =>
-  <StyledWrapper
-    css={iconList}
-    component={List}
+  <List
+    className={styles['icon-list']}
     noBullets
     condensed
     horizontal={{ spaceBetween: 6 }}
   >
     {
-        socialMediaData.map(company =>
-          <LinkWrapper
-            key={`social-media-${company.name}`}
-            href={company.href}
-          >
-            <Icon
-              icon={company.name}
-              height='28px'
-              width='28px'
-            />
-          </LinkWrapper>
-        )
-      }
-  </StyledWrapper>
+      socialMediaData.map(company =>
+        <LinkWrapper
+          key={`social-media-${company.name}`}
+          href={company.href}
+        >
+          <Icon
+            icon={company.name}
+            height='28px'
+            width='28px'
+          />
+        </LinkWrapper>
+      )
+    }
+  </List>
 
   ;
 
