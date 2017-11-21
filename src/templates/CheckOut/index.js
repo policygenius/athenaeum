bdf89bb0a7beb2061ee542be331e09297a7c9411
John Colella <john.colella@policygenius.com>
2017-11-21 14:25:16 -0500

    Fix checkout mobile sticky header

--- src/templates/CheckOut/index.js
+++ src/templates/CheckOut/index.js
@@ -90,34 +90,42 @@ function CheckOut(props) {
             <Col
               className={classnames(styles['sticky-wrapper'], styles['head-body-wrapper'])}
             >
-              <Col
-                className={styles['head-body']}
-                fullwidth
+              <Sticky
+                className={styles['sticky']}
+                activeClass={styles['sticky']}
+                enabled
+                bottomBoundary='#sticky-bottom'
               >
-                <Layout
-                  className={styles['cost']}
-                  mediumCols={[ 12 ]}
-                  smallCols={[ 6 ]}
+                <Col
+                  className={styles['head-body']}
+                  fullwidth
                 >
-                  <Col className={styles['cost-title']}>
-                    <Icon className={styles['icon']} icon='cart' />
-                    {' '}
-                    <div>Checkout</div>
-                  </Col>
-                  <Col fullwidth className={styles['cost-price']}>
-                    { curr && <sup>{curr}</sup> }
-                    { value }
-                    { unit && <Text type={7} tag='span'>{unit}</Text> }
-                  </Col>
-                </Layout>
-              </Col>
-              { userAlert.condition &&
-                <CheckoutUserAlert
-                  enabled={false}
-                  userAlert={userAlert}
-                  className='user-alert-mobile'
-                />
+                  <Layout
+                    className={styles['cost']}
+                    mediumCols={[ 12 ]}
+                    smallCols={[ 6 ]}
+                  >
+                    <Col className={styles['cost-title']}>
+                      <Icon className={styles['icon']} icon='cart' />
+                      {' '}
+                      <div>Checkout</div>
+                    </Col>
+                    <Col fullwidth className={styles['cost-price']}>
+                      { curr && <sup>{curr}</sup> }
+                      { value }
+                      { unit && <Text type={10} font='b' tag='span'>{unit}</Text> }
+                    </Col>
+                  </Layout>
+                </Col>
+                {
+                  userAlert.condition &&
+                    <CheckoutUserAlert
+                      enabled={false}
+                      userAlert={userAlert}
+                      className='user-alert-mobile'
+                    />
                 }
+              </Sticky>
             </Col>
 
             <Col className={styles['head-foot']}>
