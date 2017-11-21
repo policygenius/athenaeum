5eba62a15516f9cbd1230a4a24a01a20d39c8a36
Dan Novograd <dnovograd@gmail.com>
2017-11-20 22:28:36 -0500

    Update policy card and add back icon

--- src/organisms/cards/FeaturedPolicyCard/ButtonGroup.js
+++ src/organisms/cards/FeaturedPolicyCard/ButtonGroup.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Col from 'atoms/Layout/Col';
 import Button from 'atoms/Button';
-import Layout from 'atoms/Layout';
-import LinkWrapper from 'atoms/LinkWrapper';
 import Spacer from 'atoms/Spacer';
 
 import styles from './featured_policy_card.module.scss';
@@ -12,66 +9,34 @@ import styles from './featured_policy_card.module.scss';
 function ButtonGroup(props) {
   const {
     onDetails,
-    onContinue,
     onCompare,
-    continueCTAText,
   } = props;
 
   return (
     <div className={styles['button-group']}>
       <Button
         variant='action'
-        onClick={onContinue}
+        onClick={onDetails}
       >
-        {continueCTAText}
+        View Policy
       </Button>
-
-      <Spacer spacer={2} />
-
       {
-        onDetails &&
-          <LinkWrapper
-            onClick={onDetails}
-            className={styles['details-link']}
+        onCompare &&
+        <div>
+          <Spacer spacer={2} />
+          <Button
+            onClick={onCompare}
           >
-            {'Details & Review'}
-          </LinkWrapper>
-      }
-
-      {
-        onDetails && onCompare &&
-          <Layout
-            smallCols={[ 6 ]}
-            className={styles['secondary-actions-wrapper']}
-            fullwidth
-          >
-            <Col
-              className={styles['secondary-action']}
-            >
-              <Button
-                onClick={onDetails}
-              >
-                Details
-              </Button>
-            </Col>
-            <Col
-              className={styles['secondary-action']}
-            >
-              <Button
-                onClick={onCompare}
-              >
-                Compare
-              </Button>
-            </Col>
-          </Layout>
+            Compare
+          </Button>
+        </div>
       }
     </div>
   );
 }
 
 ButtonGroup.propTypes = {
-  onDetails: PropTypes.func,
-  onContinue: PropTypes.func.isRequired,
+  onDetails: PropTypes.func.isRequired,
   onCompare: PropTypes.func,
   continueCTAText: PropTypes.string,
 };
