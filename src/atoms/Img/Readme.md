e7d681f7a07db6ed913215dcfccbc419b506b433
Drew Fantastic <andrew.joia@policygenius.com>
2017-11-22 16:29:14 -0500

    Add picture source support

--- src/atoms/Img/Readme.md
+++ src/atoms/Img/Readme.md
@@ -5,3 +5,15 @@ The `<Img />` component can take also any prop that an HTML5 img can take.
 ```jsx
     <Img />
 ```
+
+#### Using different pictures for each view size (mobile, tablet, desktop)
+If you use these params, be sure to include either a `src` or and `imgixSrc`
+
+```jsx
+<Img
+  mobileSrc='https://learnedia.com/wp-content/uploads/2017/02/cute-dog.jpg'
+  tabletSrc='https://learnedia.com/wp-content/uploads/2017/02/puppy.jpg'
+  desktopImgixSrc='placeholders/stocksy-medium-2.jpg'
+  imgixSrc='placeholders/stocksy-medium-2.jpg'
+/>
+```
--- src/atoms/Img/index.js
+++ src/atoms/Img/index.js
@@ -5,6 +5,11 @@ import styles from './img.module.scss';
 
 const strParams = '&fit=clip&auto=compress&auto=format&ch=Width,DPR,Save-Data';
 
+const imgixSrcStr = (src, width = 768, height = 768) => `https://policygenius-images.imgix.net/${src}` +
+  `?w=${Math.min(width, width)}` +
+  `&h=${Math.min(height, height)}` +
+  '&fit=clip&auto=format&auto=compress';
+
 const imgixSrcset = ( src, maxWidth = Infinity, maxHeight = Infinity ) => `
   https://policygenius-images.imgix.net/${src}?w=${Math.min(maxWidth, 1025)}&h=${Math.min(maxHeight, 1025)}${strParams} 1026w,
   https://policygenius-images.imgix.net/${src}?w=${Math.min(maxWidth, 768)}&h=${Math.min(maxHeight, 768)}${strParams} 769w,
@@ -25,8 +30,21 @@ function Img(props) {
     title,
     maxWidth = Infinity,
     maxHeight = Infinity,
+    mobileSrc,
+    tabletSrc,
+    desktopSrc,
+    mobileImgixSrc,
+    tabletImgixSrc,
+    desktopImgixSrc,
     ...rest
   } = props;
+
+  const isPicture = mobileSrc ||
+    tabletSrc ||
+    desktopSrc ||
+    mobileImgixSrc ||
+    tabletImgixSrc ||
+    desktopImgixSrc;
 
   const classes = cx(
     styles['img'],
@@ -37,20 +55,34 @@ function Img(props) {
     if (srcSet) { return srcSet; }
     if (src) { return undefined; }
 
-    return imgixSrcset(`${imgixSrc}`, maxWidth);
+    return imgixSrcset(`${imgixSrc}`, maxWidth, maxHeight);
   })();
 
-  const imgixSrcStr = `https://policygenius-images.imgix.net/${imgixSrc}` +
-    `&w=${Math.min(maxWidth, 768)}` +
-    `&h=${Math.min(maxHeight, 768)}` +
-    '?fit=clip&auto=format&auto=compress';
+  if (isPicture) {
+    const defaultSrc = src || imgixSrcStr(imgixSrc);
+
+    return (
+      <picture className={className}>
+        { ( mobileImgixSrc || mobileSrc ) && <source srcSet={mobileSrc || imgixSrcStr(mobileImgixSrc, 320)} media='(max-width: 320px)' />}
+        { ( tabletImgixSrc || tabletSrc ) && <source srcSet={tabletSrc || imgixSrcStr(tabletImgixSrc, 768)} media='(max-width: 768px)' />}
+        { ( desktopImgixSrc || desktopSrc ) && <source srcSet={desktopSrc || imgixSrcStr(desktopImgixSrc, 1025)} media='(min-width: 768px)' />}
+        <img
+          className={styles.img}
+          alt={alt || createName(src || imgixSrc)}
+          title={title || createName(src || imgixSrc)}
+          src={defaultSrc}
+          {...rest}
+        />
+      </picture>
+    );
+  }
 
   return (
     <img
       className={classes}
-      alt={alt || createName(imgixSrc || src)}
-      title={title || createName(imgixSrc || src)}
-      src={src || imgixSrcStr}
+      alt={alt || createName(src || imgixSrc)}
+      title={title || createName(src || imgixSrc)}
+      src={src || imgixSrcStr(imgixSrc)}
       srcSet={srcset}
       sizes='100vw'
       {...rest}
@@ -101,6 +133,31 @@ Img.propTypes = {
    * Used with imgixSrc to prevent larger than necessary images from loading
    */
   maxHeight: PropTypes.string,
+
+  /**
+   * When used, will create a <source> for a <picture> element for mobile
+   */
+  mobileSrc: PropTypes.string,
+  /**
+   * When used, will create a <source> for a <picture> element for tablet
+   */
+  tabletSrc: PropTypes.string,
+  /**
+   * When used, will create a <source> for a <picture> element for desktop
+   */
+  desktopSrc: PropTypes.string,
+  /**
+   * When used, will create a <source> for a <picture> element for mobile. Will use imgix custom string.
+   */
+  mobileImgixSrc: PropTypes.string,
+  /**
+   * When used, will create a <source> for a <picture> element for tablet. Will use imgix. Will use imgix custom string.
+   */
+  tabletImgixSrc: PropTypes.string,
+  /**
+   * When used, will create a <source> for a <picture> element for desktop. Will use imgix. Will use imgix custom string.
+   */
+  desktopImgixSrc: PropTypes.string,
 };
 
 Img.defaultProps = {
