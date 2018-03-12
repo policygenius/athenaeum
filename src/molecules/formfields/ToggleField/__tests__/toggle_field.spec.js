7c53be94e90390faa1681724221f435e8bba9fc6
Drew! I'm Drew! Yay! <andrew.joia@policygenius.com>
2018-03-12 17:40:34 -0400

    Update ToggleField display logic [ch17366]
    For our Life4 EasyQuiz. We need to check that the input is actually
    undefined, not that it's falsey

--- src/molecules/formfields/ToggleField/__tests__/toggle_field.spec.js
+++ src/molecules/formfields/ToggleField/__tests__/toggle_field.spec.js
@@ -2,12 +2,29 @@ import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 
-import ToggleField from 'molecules/formfields/ToggleField';
+import ToggleField, { toBoolean } from 'molecules/formfields/ToggleField';
 
 describe('<ToggleField />', () => {
   it('renders', () => {
     const wrapper = mount(<ToggleField input={{}} />);
 
     expect(wrapper.type()).to.equal(ToggleField);
+  });
+});
+
+describe('toBoolean', () => {
+  it('returns true correctly', () => {
+    expect(toBoolean('true')).to.equal(true);
+    expect(toBoolean(true)).to.equal(true);
+  });
+
+  it('returns false correctly', () => {
+    expect(toBoolean('false')).to.equal(false);
+    expect(toBoolean(false)).to.equal(false);
+  });
+
+  it('returns the value correctly', () => {
+    expect(toBoolean('anything')).to.equal('anything');
+    expect(toBoolean(1)).to.equal(1);
   });
 });
