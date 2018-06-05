041995180cdd2595e32e45cbb3059c2e54fab9b6
Trevor Nelson <trevor.nelson@policygenius.com>
2018-06-05 18:45:01 -0400

    Fix StarRating filled in stars styles

--- src/molecules/StarRating/index.js
+++ src/molecules/StarRating/index.js
@@ -1,5 +1,6 @@
 import React from 'react'; import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import times from 'lodash/times';
 import Icon from 'atoms/Icon';
 import styles from './star_rating.module.scss';
 
@@ -19,11 +20,23 @@ function StarRating(props) {
 
   return (
     <span className={classes}>
-      <Icon className={styles.star} icon='star' width={size} height={size} />
-      <Icon className={styles.star} icon='star' width={size} height={size} />
-      <Icon className={styles.star} icon='star' width={size} height={size} />
-      <Icon className={styles.star} icon='star' width={size} height={size} />
-      <Icon className={styles.star} icon='star' width={size} height={size} />
+      {
+        times(5, (i) => (
+          <Icon
+            key={i}
+            className={classnames(
+              styles.star,
+              {
+                [styles['filled-in']]: i < rating
+              }
+            )}
+            icon='star'
+            width={size}
+            height={size}
+            renderSVGDOM
+          />
+        ))
+      }
     </span>
   );
 }
