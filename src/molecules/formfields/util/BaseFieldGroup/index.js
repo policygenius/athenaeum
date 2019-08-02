6cc3fc6cd33d85b96be16574b5b67385940abc73
jl08 <jenny.lee@policygenius.com>
2019-08-02 11:00:53 -0400

    readd bas field group

---
+++ src/molecules/formfields/util/BaseFieldGroup/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable react/require-render-return */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class BaseFieldGroup extends React.Component {
+  static propTypes = {
+    input: PropTypes.object,
+    meta: PropTypes.object,
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside, false);
+  }
+
+  wrapperReference = null;
+
+  clickIsLeaving = true;
+
+  handleClickOutside = (e) => {
+    const { input } = this.props;
+
+    if (!this.wrapperReference) { return; }
+    if (!this.wrapperReference.contains(e.target)) {
+      input.onBlur();
+    }
+  }
+
+  handleMouseDown = (e) => {
+    if (this.wrapperReference.contains(e.currentTarget)) {
+      this.clickIsLeaving = false;
+    }
+  }
+
+  handleMouseUp = () => {
+    this.clickIsLeaving = true;
+  }
+
+  handleBlur = () => {
+    const { input } = this.props;
+
+    if (this.clickIsLeaving) {
+      input.onBlur();
+    }
+  }
+
+  render() {
+    throw Error('Child element should have its own call to .render');
+  }
+}
+
+export default BaseFieldGroup;
